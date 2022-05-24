d94c2bcaec373428e181a31d5669369297e02df5
Eli <eliunited@gmail.com>
2022-05-23 19:10:19 -0700

    who knows

--- src/routes/photos.json.js
+++ src/routes/photos.json.js
@@ -3,16 +3,7 @@ import { sortBy } from 'underscore'
 import { format } from 'date-fns'
 
 export async function get() {
-  const opts = { method: 'GET', headers: {} }
-
-  opts.headers['X-Auth-Email'] = 'eliunited@gmail.com'
-  opts.headers['X-Auth-Key'] = 'd513e7aa48e29e4371190a0ea4cec21232067'
-  opts.headers['Content-Type'] = 'application/json'
-
-  const base = 'https://api.cloudflare.com/client/v4/accounts/'
-  const accountID = '7031c3a579be0b63ef6be8e0eeb6d156'
-  const response = await fetch(`${base}/${accountID}/images/v1?page=1&per_page=100`, opts)
-
+ 
   const {
     result: { images }
   } = await response.json()
--- src/routes/photos.svelte
+++ src/routes/photos.svelte
@@ -1,29 +1,32 @@
-<script context="module">
-  export async function load({ fetch }) {
-    const url = `/photos.json`
-    const res = await fetch(url)
-
-    if (res.ok) {
-      return {
-        status: res.status,
-        props: {
-          images: await res.json()
-        }
-      }
-    }
-
-    return {
-      status: res.status,
-      error: new Error(`Could not load ${url}`)
-    }
-  }
-</script>
-
 <script>
   import Gallery from 'svelte-brick-gallery'
-  export let images
+
+  let images = [
+    { src: 'public/images/1.jpg' },
+    // { src: 'public/images/2.jpg' },
+    // { src: 'public/images/3.jpg' },
+    // { src: 'public/images/4.jpg' },
+    // { src: 'public/images/5.jpg' },
+    // { src: 'public/images/6.jpg' },
+    // { src: 'public/images/7.jpg' },
+    // { src: 'public/images/8.jpg' },
+    // { src: 'public/images/9.jpg' },
+    // { src: 'public/images/10.jpg' },
+    // { src: 'public/images/11.jpg' },
+    // { src: 'public/images/12.jpg' },
+    // { src: 'public/images/13.jpg' },
+    // { src: 'public/images/14.jpg' },
+    // { src: 'public/images/15.jpg' },
+    // { src: 'public/images/16.jpg' },
+    // { src: 'public/images/17.jpg' },
+    // { src: 'public/images/18.jpg' },
+    // { src: 'public/images/19.jpg' },
+    // { src: 'public/images/20.jpg' },
+    // { src: 'public/images/21.jpg' },
+    { src: 'public/images/22.jpg' }
+  ]
 </script>
 
 <Gallery {images} gap={0} itemHeight={340} />
 
-<img src="/src/images/1.jpg" />
+<!-- <img src="public/images/1.jpg" /> -->
